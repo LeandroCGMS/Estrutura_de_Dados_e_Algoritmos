5c82b83db75e1748e4f3854dfbd352d7b11a9827
leandro <leandro@computador-pessoal-do-leandro>
2018-03-14 18:30:28 -0400

    Aula3, algoritmo eficiente para adicionar elemento no vetor.

--- Estrutura-de-Dados-e-AlgoritmosGit/src/com/leandro/estruturadados/vetor/Vetor.java
+++ Estrutura-de-Dados-e-AlgoritmosGit/src/com/leandro/estruturadados/vetor/Vetor.java
@@ -3,8 +3,41 @@ package com.leandro.estruturadados.vetor;
 public class Vetor {
 	
 	private String[] elementos;
+	private int tamanho;
 	
 	public Vetor(int capacidade) {
 		this.elementos =  new String[capacidade];
+		this.tamanho = 0;
+	}
+	
+	/*public void adiciona(String elemento) {
+		for(int i = 0; i < this.elementos.length; i++) {
+			if(this.elementos == null) {
+				this.elementos[i] = elemento;
+				break;
+			}
+		}
+	}*/
+	
+	/*public void adiciona(String elemento) throws Exception {
+		if(this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+		} else {
+			throw new Exception("Vetor já está cheio. Não é possível adicionar mais elementos.");
+		}
+		
+	}*/
+	
+	public boolean adiciona(String elemento) {
+		if(this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			System.out.println("Elemento adicionado com sucesso");
+			return true;
+		} 
+		System.out.println("Falha: não há mais espaço para adicionar novo elemento.");
+		return false;
+		
 	}
 }
---
+++ Estrutura-de-Dados-e-AlgoritmosGit/src/com/leandro/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,19 @@
+package com.leandro.estruturadados.vetor.teste;
+
+import com.leandro.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(2);
+
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+
+
+
+	}
+
+}
