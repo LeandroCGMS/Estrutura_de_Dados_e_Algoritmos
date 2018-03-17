b7d75d97ec678aad8522579573a686d9df44c294
leandro <leandro@computador-pessoal-do-leandro>
2018-03-17 17:27:57 -0400

    Correção do método ultimoIndice().

--- Estrutura-de-Dados-e-AlgoritmosGit/src/com/leandro/estruturadados/vetor/labs/Lista.java
+++ Estrutura-de-Dados-e-AlgoritmosGit/src/com/leandro/estruturadados/vetor/labs/Lista.java
@@ -83,7 +83,7 @@ public class Lista<T> {
 	}
 	
 	public int ultimoIndice(T elemento) {
-		for(int i = this.tamanho-1; i >= 0 ; i++) {
+		for(int i = this.tamanho-1; i >= 0 ; i--) {
 			if(this.elementos[i].equals(elemento)){
 				return i;
 			}
