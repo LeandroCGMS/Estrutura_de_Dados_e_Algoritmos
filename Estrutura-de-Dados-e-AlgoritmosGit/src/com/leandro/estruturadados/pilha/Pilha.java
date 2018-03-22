6418898b52f97da2984a3b40d8ddd4fdbce76e37
leandro <leandrocgms1986#outlook.com>
2018-03-22 18:06:34 -0400

    Aulas 16 e 17.

--- Estrutura-de-Dados-e-AlgoritmosGit/src/com/leandro/estruturadados/pilha/Pilha.java
+++ Estrutura-de-Dados-e-AlgoritmosGit/src/com/leandro/estruturadados/pilha/Pilha.java
@@ -14,4 +14,26 @@ public class Pilha<T> extends EstruturaEstatica<T> {
 	public void empilha(T elemento) {
 		super.adiciona(elemento);
 	}
+	
+	public T topo() {
+		
+		if(this.estaVazia()) {
+			return null;
+		}
+		return this.elementos[tamanho - 1];
+	}
+	
+	public T desempilha() {
+		
+		if(this.estaVazia()) {
+			return null;
+		}
+		
+		/*T elemento = this.elementos[tamanho -1];
+		tamanho--;
+		
+		return elemento;*/
+		
+		return this.elementos[--tamanho];
+	}
 }
