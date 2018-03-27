af56d02acf22fbaa414b643d1c9c2f24340543be
leandro <leandrocgms1986#outlook.com>
2018-03-27 13:12:37 -0400

    Exer08 da Aula18. É nesse nível de raciocínio que desejo chegar. Talvez algum dia.

--- Estrutura-de-Dados-e-AlgoritmosGit/src/com/leandro/aula18/labs/Exer08.java
+++ Estrutura-de-Dados-e-AlgoritmosGit/src/com/leandro/aula18/labs/Exer08.java
@@ -4,112 +4,108 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Exer08 {
+	
 	private static Scanner scan = new Scanner(System.in);
+	private static int cont = 0;
 
 	public static void main(String[] args) {
+		Stack<Integer> ori = new Stack<Integer>();
+		Stack<Integer> aux = new Stack<Integer>();
+		Stack<Integer> dest = new Stack<Integer>();
 
-		Stack<Integer> pino1 = new Stack<Integer>();
-		Stack<Integer> pino2 = new Stack<Integer>();
-		Stack<Integer> pino3 = new Stack<Integer>();
-
-		final int ANILHA1 = 8;
-		final int ANILHA2 = 6;
-		final int ANILHA3 = 4;
-		final int ANILHA4 = 2;
-
-		pino1.push(ANILHA1);
-		pino1.push(ANILHA2);
-		pino1.push(ANILHA3);
-		pino1.push(ANILHA4);
-
-		for(int i = 0; i < 4; i++) {
-			pino2.push(0);
-			pino3.push(0);
-		}
-
-		boolean opValida = false;
-		String op = "";
-		int opc = 0;
-		while(!opValida) {
-			System.out.println("No pino 1 tem 4 anilhas com peso 8, 6, 4 e 2 quilos"
-					+ ", de baixo para cima, pinos da esquerda para direita."
-					+ "\nVocê quer passar essas anilhas para o pino 2 ou 3? Digite 2 ou 3.\n");
-			for(int i = pino1.size() -1; i >=0; i--) {
-				System.out.println("\t[" + pino1.get(i) + "]\t[ ]\t[ ]\n");
-			}
-			System.out.println("====================================\0");
-			op = scan.nextLine();
+		boolean respostaValida = false;
+		int qtdDiscos = 0;
+		while (!respostaValida) {
+			System.out.println("Quantos discos quer inserir na primeira torre?");
+			String resposta = scan.nextLine();
 			try {
-				opc = Integer.parseInt(op);
-				if(opc < 2 || opc > 3) {
-					throw new Exception();
-				} else {
-					opValida = true;
-				}
+				qtdDiscos = Integer.parseInt(resposta);
+				respostaValida = true;
+			} catch (Exception e) {
+				System.out.println("Resposta não é um inteiro ou não é um inteiro dentro do limite do tipo.");
+			}
+		}
+		
+		for(int i = qtdDiscos; i > 0; i--) {
+			ori.push(i);
+		}
 
-			} catch(Exception e) {
-				System.out.println("Opção inválida. Tente novamente.");
+		torreDeHanoi(qtdDiscos, ori,dest,aux);
+		
+		System.out.println("Exibindo a torre 3 completa.");
+		for (int i = dest.size() - 1; i >= 0; i--) {
+			System.out.println("| " + dest.get(i) + " |");
+		}
+
+	}
+	
+	public static void tiraDaOriPoeNaDest(Stack<Integer> ori, Stack<Integer> dest, Stack<Integer> aux) {
+		dest.push(ori.peek());
+		ori.pop();
+	}
+	
+	public static void tiraDaOriPoeNaAux(Stack<Integer> ori, Stack<Integer> dest, Stack<Integer> aux) {
+		aux.push(ori.peek());
+		ori.pop();
+	}
+	
+	public static void tiraDaAuxPoeNaOri(Stack<Integer> ori, Stack<Integer> dest, Stack<Integer> aux) {
+		ori.push(aux.peek());
+		aux.pop();
+	}
+	
+	public static void tiraDaAuxPoeNaDest(Stack<Integer> ori, Stack<Integer> dest, Stack<Integer> aux) {
+		dest.push(aux.peek());
+		aux.pop();
+	}
+	
+	public static void tiraDaDestPoeNaOri(Stack<Integer> ori, Stack<Integer> dest, Stack<Integer> aux) {
+		ori.push(dest.peek());
+		dest.pop();
+	}
+	
+	public static void tiraDaDestPoeNaAux(Stack<Integer> ori, Stack<Integer> dest, Stack<Integer> aux) {
+		aux.push(dest.peek());
+		dest.pop();
+	}
+
+	public static void torreDeHanoi(int qtdDiscos, Stack<Integer> ori, Stack<Integer> dest, Stack<Integer> aux) {
+
+		if(qtdDiscos == 1) {
+			cont++;
+			System.out.println(cont + "º movimento.");
+			tiraDaOriPoeNaDest(ori,dest,aux);
+		} else {
+			torreDeHanoi(qtdDiscos - 1,ori,aux,dest);
+			cont++;
+			System.out.println(cont + "º movimento.");
+			tiraDaOriPoeNaDest(ori,dest,aux);
+			torreDeHanoi(qtdDiscos - 1,aux,dest,ori);
+		}
+
+		
+
+		
+
+	}
+
+	public static boolean torre3EstaCompleta(Stack<Integer> ori, Stack<Integer> dest, Stack<Integer> aux) {
+		int contPosicoesCompletas = 0;
+		for (int i = dest.size() - 1; i > 0; i--) {
+
+			if (dest.get(i) == null) {
+				return false;
+			}
+			if (dest.get(i) < dest.get(i - 1)) {
+				contPosicoesCompletas++;
 			}
 		}
 
-		switch(opc) {
-		case 2:
-			while(!pino1.isEmpty()) {
-				if(pino1.peek() < pino2.peek()) {
-					pino2.push(pino1.peek());
-					pino1.pop();
-				} else if(pino1.peek() < pino3.peek()){
-					pino3.push(pino1.peek());
-					pino1.pop();
-				} else if(pino1.peek() > pino2.peek() && pino1.peek() > pino3.peek()) {
-					if(pino2.peek() < pino3.peek()) {
-						pino3.push(pino2.peek());
-						pino2.pop();
-						pino2.push(pino1.peek());
-						pino1.pop();
-					} else if(pino2.peek() > pino3.peek()) {
-						pino2.push(pino3.peek());
-						pino3.pop();
-						pino1.push(pino3.peek());
-						pino3.pop();
-						
-						pino3.push(pino2.peek());
-						pino2.pop();
-						pino3.push(pino1.peek());
-						pino1.pop();
-						pino3.push(pino2.peek());
-						pino2.pop();
-						pino3.push(pino1.peek());
-						pino1.pop();
-						
-						pino2.push(pino3.peek());
-						pino3.pop();
-						pino2.push(pino3.peek());
-						pino3.pop();
-						pino2.push(pino3.peek());
-						pino3.pop();
-						pino2.push(pino3.peek());
-						pino3.pop();
-						
-						
-					}
-					
-					
-				}
-			}	
-			
-			for(int i = 0; i < pino2.size(); i++) {
-				System.out.println(pino2.get(i));
-			}
-
-			break;
-		case 3:
-			break;
+		if (contPosicoesCompletas == dest.size() - 1) {
+			return true;
 		}
 
-
-
-
+		return false;
 	}
 
 }
